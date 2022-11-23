c1f16b20bb49ce132690ef397b1e7ccd954b0b67
DarioDa86 <dario.dagnelli@gmail.com>
2022-11-23 10:43:51 +0100

    creata struttura base, pagina prodotti + css e js

---
+++ layout-parts/footer.php
@@ -0,0 +1,11 @@
+    <footer class="bg-dark">
+        <p class="container">prova footer</p>
+    </footer>
+
+    <script src="https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://bootswatch.com/_vendor/prismjs/prism.js"></script>
+
+    <script src="./assets/js/main.js"></script>
+    </body>
+
+    </html>
---
+++ pages/homepage.php
@@ -0,0 +1,3 @@
+<h1>benvenuti</h1>
+<p class="lead">Trova il tuo prodotto sul nostro e-commerce</p>
+<a href="#" class="btn btn-primary">Go somewhere</a>
